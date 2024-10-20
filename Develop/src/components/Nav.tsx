1441c7ad2beeb9710df4eb6d6d1c9f380e36fa91
amauri <brown817@msu.edu>
2024-10-20 11:09:03 -0400

    start

---
+++ Develop/src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+const Nav = () => {
+  // TODO: Add necessary code to display the navigation bar and link between the pages
+  return (
+      <div>
+      <Link to="/">Home</Link> 
+      <Link to="/S">Potential Candidates</Link>
+      </div>
+  
+  )
+};
+
+export default Nav;
